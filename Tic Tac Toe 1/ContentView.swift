d1e2a9947a867fcb9a00446eb7daa7878ab0b3c1
Alex Artamonov <aartamonov@bsd220.org>
2025-11-21 12:07:33 -0600

    added tap gesture recognizer

--- Tic Tac Toe 1/ContentView.swift
+++ Tic Tac Toe 1/ContentView.swift
@@ -8,28 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count:9)
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
                 .font(.title)
                 .fontWeight(.bold)
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3 )){
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Color.blue
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                            
+                    }
+                    .frame(width: 120, height: 120, alignment:.center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        moves[index] = "X"
+                    }
+                    
+                }
             }
+            .preferredColorScheme(.dark)
+            
         }
-        .preferredColorScheme(.dark)
-        
     }
 }
-
 #Preview {
     ContentView()
 }
