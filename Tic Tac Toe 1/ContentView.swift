6c4bbe7fd48b88cfed869f8ad44d122b1af6676b
Alex Artamonov <aartamonov@bsd220.org>
2025-11-21 09:03:37 -0600

    Initial Commit

---
+++ Tic Tac Toe 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe 1
+//
+//  Created by Alex Artamonov on 11/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic Tac Toe 1/Tic_Tac_Toe_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_1App.swift
+//  Tic Tac Toe 1
+//
+//  Created by Alex Artamonov on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
